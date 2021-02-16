e4dc06903dd79643331894fe81e427f40cbd5f9a
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-02-16 11:32:48 +0100

    post joi

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const Joi = require('joi');
 const port = process.env.PORT || 3000;
 const app = express();
-
+app.use(express.json())
 var students = [
     {id : 1, name : "student 1"},
     {id : 2, name : "student 2"},
@@ -16,6 +17,23 @@ app.get('/api/students/:id',(req,res) => {
     let student = students.find(s => s.id === parseInt(req.params.id))
     if(!student) 
         return res.status(404).send(`Student with id : ${req.params.id} is not found`);
+    res.send(student);
+});
+
+const validation_schema = {
+    name : Joi.string().min(3).max(50).required()
+}
+app.post('/api/students', (req,res)=> {
+    //if(!req.body.name || req.body.name.length < 3)
+    //   return res.status(400).send('Name is not found or length < 3')
+    let valid_res = Joi.validate(req.body,validation_schema);
+    if(valid_res.error)
+        return res.status(400).send(valid_res.error.details[0].message)
+    let student = {
+        id: students.length + 1,
+        name : req.body.name
+    };
+    students.push(student);
     res.send(student);
 });
 
